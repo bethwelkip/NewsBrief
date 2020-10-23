d76c34b274d19b7bc0e32b027c6b7d5aac7ed459
Bethwelkip <bkiplimo@princeton.edu>
2020-10-23 06:37:45 -0400

    add articles portion of views

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 
 from app import app
 from .request import get_sources
+from .request import get_articles
 
 # home page
 @app.route('/')
@@ -11,9 +12,19 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-     title = "Your One Stop News Site"
+     title = "Your Everyday News Bank"
      sources = get_sources()
      #news sources array here, passed then for-looped
      return render_template('index.html', title = title, sources = sources)
 
-    
+
+#articles
+@app.route('/articles/<names>')
+def articles(names):
+     '''
+     View root page function that returns the index page and its data
+     '''
+     name = names 
+     print("here")
+     articles = get_articles(names)
+     return render_template('articles.html', name = names, articles = articles)
