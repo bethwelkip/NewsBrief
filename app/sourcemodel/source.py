c316589c3ab0ca4480080980f19ef6c1c77fab4b
Bethwelkip <bkiplimo@princeton.edu>
2020-10-23 06:38:08 -0400

    add source model

--- app/sourcemodel/source.py
+++ app/sourcemodel/source.py
@@ -3,7 +3,8 @@ class Source:
     defines sources of news
     '''
     sources = []
-    def __init__(self,name, description,url, category):
+    def __init__(self,id, name, description,url, category = ""):
+        self.id = id
         self.name = name
         self.description = description
         self.url = url
